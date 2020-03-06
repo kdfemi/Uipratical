5eb4b9f1a7bdf180c03d5170953ea27ded02d1ef
Kolade Femi <koladefemi@gmail.com>
2020-03-06 14:25:23 +0100

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ViewChild, ElementRef, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'practicalTest';
+  imageUrl = ['/assets/image1.png', '/assets/image2.png', '/assets/image3.png', '/assets/image4.png'];
+  backgroundColors = ['#0C182A', '#492901', '#38001C', '#1C081C'];
+  presentTab = 0;
+
+  @ViewChild('imageDiv') imageDiv: ElementRef<HTMLDivElement>;
+  @ViewChild('wrapper') wrapper: ElementRef<HTMLDivElement>;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    this.switchImageAndBackgroundColor(0);
+  }
+  switchImageAndBackgroundColor(imageId: number, event?: Event) {
+    const previousTab = this.presentTab;
+    this.presentTab = imageId;
+    this.renderer.setStyle(this.imageDiv.nativeElement, 'background-image', `url('${this.imageUrl[imageId]}')`);
+    this.renderer.setStyle(this.wrapper.nativeElement, 'background-color', `${this.backgroundColors[imageId]}`);
+    if (event) {
+      if (previousTab > this.presentTab) {
+        // this.renderer.
+      }
+    }
+  }
 }
